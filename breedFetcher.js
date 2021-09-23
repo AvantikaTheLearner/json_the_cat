9df42940555aa6fc005055f055b50f03f156fd41
AvantikaTheLearner <avantikaagarwaljhs@gmail.com>
2021-09-23 18:03:22 -0400

    Refactoring of code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,19 @@
 const request = require('request');
 
-request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
+const breedName = process.argv.slice(2).join();
+
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+  if (error) {
+    console.log('error:', error); // Print the error if one occurred
+  }
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
   
-  //console.log(typeof body);
   const data = JSON.parse(body);
-  //console.log(data);
-  //console.log(typeof data);
-  console.log(data[0].description);
-  data[0].name = args.join();
-  console.log(data);
-  if (data[0].name === '') {
+  if (!data[0]) {
     console.log("Breed not found");
+  } else {
+    console.log(data[0].description);
   }
-  
 });
-const args = process.argv.slice(2);
-console.log(args);
